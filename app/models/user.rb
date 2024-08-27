bb94138eadebf3dd2f54c048db5ea63b9eced111
ntrung2006 <nguyen.trung@moneyforward.vn>
2024-08-27 11:42:34 +0700

    Remove custome email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,8 +39,6 @@ class User < ApplicationRecord
          :recoverable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  validates :email, presence: true, email: true
-
   validates :uid, uniqueness: { scope: :provider }
 
   attribute :impersonated_by, :integer
